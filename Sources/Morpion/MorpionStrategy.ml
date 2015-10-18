07369ad1b4a0f35e61a79962bf674ed3bbd5dcd5
Simon Bihel <simon.bihel@outlook.com>
2015-10-18 18:41:39 +0200

    [Morpion] AlphaBeta not giving errors but not quite working.

--- Sources/Morpion/MorpionStrategy.ml
+++ Sources/Morpion/MorpionStrategy.ml
@@ -1,4 +1,5 @@
 exception ImpossibleAction
+exception Sprune of (MorpionMechanism.move * float)
 
 let learningStrategyInitialized = ref(false)
 
@@ -13,9 +14,63 @@ let humanPlayerStrategy player situation =
 	MorpionGUI.displayMove player;
 	MorpionGUI.getNearestMove player situation
 
+
 let initializeLearningStrategy fileName =
 	MorpionLearning.loadFromFile fileName
-		
+
+
+let evalSituation player situation = 
+  if MorpionMechanism.gameEnded situation then (
+    if MorpionMechanism.hasWin player situation then infinity
+    else (
+        if MorpionMechanism.tie situation then 0.
+        else neg_infinity
+      )
+    )
+  else 0.
+
+
+let alphabeta player situation = 
+  let rec alpbet s a b depth player' = 
+    let a' = ref a
+    and b' = ref b in
+    let bst_mv = ref (MorpionMechanism.Move(-1,-1)) in
+    if MorpionMechanism.gameEnded s || depth = 0 then 
+      ( !bst_mv, evalSituation player situation)
+    else ( try ( 
+      let l = MorpionMechanism.getPossibleMoves s in
+      if player = player' then (
+        let v = ref infinity in
+        for i = 0 to ((List.length l)-1)
+        do
+          let s' = MorpionMechanism.play player' s (List.nth l i)
+          and mv = (List.nth l i) in
+          v := min !v (snd (alpbet s' !a' !b' (depth-1) (MorpionMechanism.otherPlayer player')));
+          if !v <= !a' then raise (Sprune ( !bst_mv, !v));
+          if !b' > !v then bst_mv := mv;
+          b' := min !b' !v
+        done;
+        ( !bst_mv, !v) 
+        )
+      else (
+        let v = ref neg_infinity in
+        for i = 0 to ((List.length l)-1)
+        do
+          let s' = MorpionMechanism.play player' s (List.nth l i)
+          and mv = (List.nth l i) in
+          v := max !v (snd (alpbet s' !a' !b' (depth-1) (MorpionMechanism.otherPlayer player')));
+          if !v >= !b' then raise (Sprune ( !bst_mv, !v));
+          if !a' < !v then bst_mv := mv;
+          a' := max !a' !v
+        done;
+        ( !bst_mv, !v)
+        )
+      )
+      with | Sprune res -> res )
+  in
+  fst (alpbet situation neg_infinity infinity 100 player)
+
+
 let learningStrategy player situation =
 (*	if not(!learningStrategyInitialized)
 	then 
@@ -24,4 +79,5 @@ let learningStrategy player situation =
 		learningStrategyInitialized := true
 		end;
 	MorpionLearning.bestPolitic situation*)
-	randomStrategy player situation
+	(* randomStrategy player situation *)
+	alphabeta player situation
--- Sources/Morpion/MorpionStrategy.mli
+++ Sources/Morpion/MorpionStrategy.mli
@@ -6,5 +6,8 @@ val randomStrategy : MorpionMechanism.player -> MorpionMechanism.situation -> Mo
 (** Stratégie pour le joueur humain : étant donné un joueur et une situation, retourne un coup récupéré à la souris*)
 val humanPlayerStrategy : MorpionMechanism.player -> MorpionMechanism.situation -> MorpionMechanism.move
 
+(** Strategie alphabeta *)
+val alphabeta : MorpionMechanism.player -> MorpionMechanism.situation -> MorpionMechanism.move
+
 (** Stratégie apprise : étant donné un joueur et une situation, retourne le meilleur coup appris*)
 val learningStrategy : MorpionMechanism.player -> MorpionMechanism.situation -> MorpionMechanism.move
