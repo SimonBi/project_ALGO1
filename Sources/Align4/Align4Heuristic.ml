2ba6efef6858f82a388c5536e57fc4314c808e4b
Simon Bihel <simon.bihel@outlook.com>
2015-10-19 21:45:39 +0200

    [Align4] Copied the functions of the morpion so I guess there's just to do a good heuristic function

--- Sources/Align4/Align4Heuristic.ml
+++ Sources/Align4/Align4Heuristic.ml
@@ -4,3 +4,12 @@ let nullHeuristic s p =
 	else if Align4Mechanism.hasWin (Align4Mechanism.otherPlayer p) s 
 	then -10000.
 	else 0.
+
+
+let evaluate_situation situation player =
+  if Align4Mechanism.hasWin player situation then infinity
+  else if Align4Mechanism.hasWin (Align4Mechanism.otherPlayer player) situation then neg_infinity
+  else if Align4Mechanism.tie situation then 0.
+  else (
+    0.
+  )
--- Sources/Align4/Align4Strategy.ml
+++ Sources/Align4/Align4Strategy.ml
@@ -1,3 +1,5 @@
+exception Sprune of (Align4Mechanism.move * float)
+
 let randomStrategy player s =
 	let l = Align4Mechanism.getPossibleMoves s
 	in 
@@ -12,5 +14,48 @@ let maxDepth = 6
 
 type state={situation : Align4Mechanism.situation;depth : int;player : Align4Mechanism.player}
 
+
+let alphabeta evalSituation player situation = 
+  let rec alpbet s a b depth player' = 
+    let a' = ref a
+    and b' = ref b in
+    let bst_mv = ref (Align4Mechanism.Move (-1)) in
+    if Align4Mechanism.gameEnded s || depth = 0 then 
+      ( !bst_mv, evalSituation s player)
+    else ( try ( 
+      let l = Align4Mechanism.getPossibleMoves s in
+      if player <> player' then (
+        let v = ref infinity in
+        for i = 0 to ((List.length l)-1)
+        do
+          let s' = Align4Mechanism.play player' s (List.nth l i)
+          and mv = (List.nth l i) in
+          v := min !v (snd (alpbet s' !a' !b' (depth-1) (Align4Mechanism.otherPlayer player')));
+          if !v <= !a' then raise (Sprune ( !bst_mv, !v));
+          if !b' > !v then bst_mv := mv;
+          b' := min !b' !v
+        done;
+        ( !bst_mv, !v) 
+        )
+      else (
+        let v = ref neg_infinity in
+        for i = 0 to ((List.length l)-1)
+        do
+          let s' = Align4Mechanism.play player' s (List.nth l i)
+          and mv = (List.nth l i) in
+          v := max !v (snd (alpbet s' !a' !b' (depth-1) (Align4Mechanism.otherPlayer player')));
+          if !v >= !b' then raise (Sprune ( !bst_mv, !v));
+          if !a' < !v then bst_mv := mv;
+          a' := max !a' !v
+        done;
+        ( !bst_mv, !v)
+        )
+      )
+      with | Sprune res -> res )
+  in
+  print_string "lel";
+  fst (alpbet situation neg_infinity infinity maxDepth player)
+
+
 let minimaxStrategy heuristic player s =
-	randomStrategy player s
+  alphabeta heuristic player s
